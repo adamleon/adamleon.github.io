5e5004c46a72c8c6167f9e08553c6c3735decdac
Adam Leon Kleppe <adam.l.kleppe@ntnu.no>
2021-10-10 20:13:49 +0200

    Adding script

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
-$('#img').on('mouseout', function () {
+$('#mapimg').on('mouseout', function () {
     $(this).css('-webkit-animation-play-state','paused');
     $(this).css('animation-play-state','paused');
 });
 
-$('#img').on('mouseover', function () {
+$('#mapimg').on('mouseover', function () {
     $(this).css('-webkit-animation-play-state','running');
     $(this).css('animation-play-state','running');
 })
